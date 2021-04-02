e55fec155bb5cbba032778ecff988e6dbfdb614a
trholdridge <trholdridge@gmail.com>
2021-04-01 20:17:09 -0400

    added ble command routine

--- code/v3/line_cutter_v3/line_cutter_v3.ino
+++ code/v3/line_cutter_v3/line_cutter_v3.ino
@@ -18,7 +18,7 @@ enum states {
   LANDED
 };
 int state = WAITING;
-char* stateStrings[6] = { "WAITING","ARMED","DEPLOYED","PARTIAL_DISREEF","FULL_DISREEF","LANDED" };
+char* stateStrings[6] = { "WAITING", "ARMED", "DEPLOYED", "PARTIAL_DISREEF", "FULL_DISREEF", "LANDED" };
 
 // Pins
 const int VOLTAGE_DIVIDER = A4;
@@ -36,7 +36,7 @@ HardwarePWM& hardwarePWM2 = HwPWM1;
 
 // VARIABLES WHICH MUST BE SET
 // Requirements for state transitions
-const double LIMIT_VELOCITY =   -3.0;  // meters/second
+//const double LIMIT_VELOCITY =   -3.0;  // meters/second
 const double ALTITUDE1 =        243;  // Disreefing altitudes, in meters (higher one first!!)
 const double ALTITUDE2 =        210;
 const int DISREEF1TIME =        12000;  // Max delay after ejection before first disreef, in milliseconds
@@ -74,6 +74,7 @@ unsigned long lastStateChange = 0;
 unsigned long lastDark = 0; // Last time it was below LIGHT_THRESHHOLD
 unsigned long cutStart1 = 0;
 unsigned long cutStart2 = 0;
+bool armed = false;
 int32_t pressure;  // pascals
 double altitude;  // meters
 double previousAltitudeAvg;
@@ -120,9 +121,9 @@ IMU imu{}; // accelerometer
 imu_out_t accelData;
 
 /*****************************
- *        SETUP/LOOP         *
+          SETUP/LOOP
  ****************************/
- 
+
 void setup() {
   // Manually add nichrome pins to HardwarePWMs and set them
   hardwarePWM1.setResolution(8);  // Write values in range [0, 255]
@@ -132,7 +133,7 @@ void setup() {
   hardwarePWM2.addPin(NICHROME_PIN2);
   hardwarePWM2.writePin(NICHROME_PIN2, 0);
   analogReadResolution(10);  // Read values in range [0, 1023]
-  
+
   Serial.begin(115200);
   //while (!Serial) { delay(10); }
 
@@ -160,12 +161,12 @@ void setup() {
   altitudeAvgDeltas.reading(0.0);
 
   // Initialize Internal File System
-  if(InternalFS.begin()) {
+  if (InternalFS.begin()) {
     Serial.println("FS initialized.");
   } else {
     Serial.println("Could not start file system.");
   }
-  
+
   // Get identifier info (board version and Bluetooth name)
   file.open(ID_FILE, FILE_O_READ);
   uint32_t readlen;
@@ -184,7 +185,7 @@ void setup() {
   SPI.setDataMode(SPI_MODE0);
   pinMode(CHIP_SELECT_PIN, OUTPUT);
   digitalWrite(CHIP_SELECT_PIN, HIGH);
-  
+
   // Set up BLE
   Bluefruit.begin(2, 0);
   Bluefruit.setTxPower(8);    // Check bluefruit.h for supported values
@@ -203,10 +204,10 @@ void setup() {
 
 
 void loop() {
-  while(millis() < loopStart + DELAY) {}
-  
+  while (millis() < loopStart + DELAY) {}
+
   loopStart = millis();  // Used for timestamps in data log
- 
+
   // Read pressure, calculate altitude
   baro.Readout();
   pressure = baro.GetPres();
@@ -221,7 +222,7 @@ void loop() {
   // Update photoresistor
   light = analogRead(PHOTO_PIN);
   // Keep track of whether it is dark or light and for how long
-  if(light < LIGHT_THRESHOLD)
+  if (light < LIGHT_THRESHOLD)
     lastDark = loopStart;
 
   // Update accelerometer
@@ -229,8 +230,16 @@ void loop() {
 
   // Update data, send to BLE centrals and flash
   updateDataStruct();
-  sendBluetoothData();
   updateFlash();
+
+  while (bleuart.available()) {
+    uint8_t ch;
+    ch = (uint8_t) bleuart.read();
+    // '!' indicates start of a command. anything else is ignored
+    if (ch == (uint8_t) '!') {
+      parse_command();
+    }
+  }
 
   if (cutStart1 > 0 && loopStart - cutStart1 > PWM_DURATION) {
     hardwarePWM1.writePin(NICHROME_PIN1, 0);
@@ -243,12 +252,17 @@ void loop() {
     Serial.println(NICHROME_PIN2);
   }
 
-  switch(state) {
+  switch (state) {
     case WAITING:
+      if (armed == true) {
+        state = ARMED;
+      }
       break;
     case ARMED:
-      if ((currentAltitudeAvg > ALTITUDE1 && currentDeltaAvg < LIMIT_VELOCITY)
-          || loopStart - lastDark > LIGHT_TRIGGER_TIME) {
+      if (armed == false) {
+        state = WAITING;
+      }
+      if (loopStart - lastDark > LIGHT_TRIGGER_TIME) {
         InternalFS.remove(STATE_FILE);
         progressState();
       }
@@ -284,15 +298,61 @@ void loop() {
 
 
 /*****************************
- *          HELPERS          *
+            HELPERS
  ****************************/
+
+void parse_command() {
+  uint8_t char_array[8];
+  int current_num = 0;
+
+  // read all text after '!' which started command
+  while ( bleuart.available() && current_num < 8 )
+  {
+    char_array[current_num] = (uint8_t) bleuart.read();
+    current_num++;
+  }
+
+  String command = (char*) char_array;
+
+  // if user typed '!cut', execute line-cutting
+  if (command.substring(0, 4).equals("help")) {
+    bleuart.write("Valid commands:\n");
+    bleuart.write("!arm, !disarm, !vars, !data");
+  }
+  else if (command.substring(0, 3).equals("arm")) {
+    if (state == WAITING) {
+      armed = true;
+      bleuart.write("Armed.");
+    } else {
+      bleuart.write("Not in waiting state.");
+    }
+  }
+  else if (command.substring(0, 6).equals("disarm")) {
+    if (state = ARMED) {
+      armed = false;
+      bleuart.write("Disarmed.");
+    } else {
+      bleuart.write("Not in armed state.");
+    }
+  }
+  else if (command.substring(0, 4).equals("vars")) {
+    sendFlightVariables();
+  }
+  else if (command.substring(0, 4).equals("data")) {
+    sendSensorData();
+  }
+  else {
+    bleuart.write("Not a valid command.\n");
+    return;
+  }
+}
 
 void startAdv(void)
 {
   // Advertising packet
   Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
   Bluefruit.Advertising.addTxPower();
-  
+
   // Include the BLE UART (AKA 'NUS') 128-bit UUID
   Bluefruit.Advertising.addService(bleuart);
 
@@ -301,21 +361,21 @@ void startAdv(void)
   Bluefruit.ScanResponse.addName();
 
   /* Start Advertising
-   * - Enable auto advertising if disconnected
-   * - Interval:  fast mode = 20 ms, slow mode = 152.5 ms
-   * - Timeout for fast mode is 30 seconds
-   * - Start(timeout) with timeout = 0 will advertise forever (until connected)
-   * 
-   * For recommended advertising interval
-   * https://developer.apple.com/library/content/qa/qa1931/_index.html   
-   */
+     - Enable auto advertising if disconnected
+     - Interval:  fast mode = 20 ms, slow mode = 152.5 ms
+     - Timeout for fast mode is 30 seconds
+     - Start(timeout) with timeout = 0 will advertise forever (until connected)
+
+     For recommended advertising interval
+     https://developer.apple.com/library/content/qa/qa1931/_index.html
+  */
   Bluefruit.Advertising.restartOnDisconnect(true);
   Bluefruit.Advertising.setInterval(32, 244);    // in unit of 0.625 ms
   Bluefruit.Advertising.setFastTimeout(30);      // number of seconds in fast mode
-  Bluefruit.Advertising.start(0);                // 0 = Don't stop advertising after n seconds  
+  Bluefruit.Advertising.start(0);                // 0 = Don't stop advertising after n seconds
 }
 
-// TODO: search linearly through last non-empty sector; don't just skip to empty one
+// TODO: search linearly through last non-empty sector; don't just skip to next empty one
 void setFlashLocation() {
   uint8_t* sector1;
   flash.read_start(0, sector1, 64);
@@ -335,7 +395,7 @@ double calibrateSeaLevel(int samples) {
   Serial.println("Reading current pressure...");
   digitalWrite(CALIBRATION_LED, HIGH);
   int sum = 0;
-  for (int i=0; i<samples; i++) {
+  for (int i = 0; i < samples; i++) {
     baro.Readout();
     sum += baro.GetPres();
     delay(100);
@@ -379,7 +439,7 @@ int pwmLevel(double targetVoltage) {
 void progressState() {
   Serial.print(state);
   Serial.print(" -> ");
-  Serial.println(state+1);
+  Serial.println(state + 1);
   Serial.println(loopStart);
   writeStateChangeData();
   lastStateChange = loopStart;
@@ -389,7 +449,7 @@ void progressState() {
 void writeStateChangeData() {
   if ( file.open(STATE_FILE, FILE_O_WRITE) ) {
     uint8_t* buffer = ( uint8_t* ) &currentData;
-    
+
     Serial.println("Writing data to file.");
     file.write(state);
     file.write(" -> ");
@@ -421,25 +481,29 @@ void updateDataStruct() {
   currentData.photoresistor = analogRead(PHOTO_PIN);
 }
 
-void sendBluetoothData() {
-  if (loopStart - lastBLE > 500) {
-      bleuart.printf("State [%s]\n", stateStrings[currentData.state]);
-      bleuart.printf("Time [%i] ", currentData.timestamp);
-  
-      bleuart.printf("Press [%u] ", currentData.pressure);
-      bleuart.printf("Temp [%f]\n", currentData.temperature);
-      bleuart.printf("Ax [%f] ", currentData.accelX);
-      bleuart.printf("Ay [%f] ", currentData.accelY);
-      bleuart.printf("Az [%f]\n", currentData.accelZ);
-  
-      bleuart.printf("Batt [%f] ", currentData.battSense * 2.0 * 3.6 / 1023.0);
-      bleuart.printf("CutSens1 [%li] ", currentData.cutSense1);
-      bleuart.printf("CutSens2 [%li]\n", currentData.cutSense2);
-      bleuart.printf("CurrSens [%li] ", currentData.currentSense);
-      bleuart.printf("Photores [%li]\n", currentData.photoresistor);
-      bleuart.println("===================");
-      lastBLE = loopStart;
-    }
+void sendSensorData() {
+  bleuart.printf("State [%s]\n", stateStrings[currentData.state]);
+  bleuart.printf("Time [%i] ", currentData.timestamp);
+
+  bleuart.printf("Press [%u Pa] ", currentData.pressure);
+  bleuart.printf("Temp [%f C]\n", currentData.temperature);
+  bleuart.printf("Ax [%f] ", currentData.accelX);
+  bleuart.printf("Ay [%f] ", currentData.accelY);
+  bleuart.printf("Az [%f]\n", currentData.accelZ);
+
+  bleuart.printf("Batt [%f] ", currentData.battSense * 2.0 * 3.6 / 1023.0);
+  bleuart.printf("CutSens1 [%li] ", currentData.cutSense1);
+  bleuart.printf("CutSens2 [%li]\n", currentData.cutSense2);
+  bleuart.printf("CurrSens [%li] ", currentData.currentSense);
+  bleuart.printf("Photores [%li]\n", currentData.photoresistor);
+}
+
+void sendFlightVariables() {
+  bleuart.printf("Cut1Alt [%f m] Cut2Alt [%f m]\n", ALTITUDE1, ALTITUDE2);
+  bleuart.printf("Cut1Time [%u ms] Cut2Time [%u ms]\n", DISREEF1TIME, DISREEF2TIME);
+  bleuart.printf("VoltPWM1 [%f V] VoltPWM2 [%u V]\n", PWM_VOLTAGE1, PWM_VOLTAGE2);
+  bleuart.printf("TimePWM [%u ms]\n", PWM_DURATION);
+  bleuart.printf("LightThreshold [%u] LightTime [%u ms]\n", LIGHT_THRESHOLD, LIGHT_TRIGGER_TIME);
 }
 
 void updateFlash() {
@@ -449,7 +513,7 @@ void updateFlash() {
       flash.write(flashLocationLocation, 0, 1);
       flashLocationLocation++;
     }
-    
+
     // Write things, increment location
     uint8_t* buffer = ( uint8_t* ) &currentData;
     flash.write(flashLocation, buffer, DATA_SIZE);
