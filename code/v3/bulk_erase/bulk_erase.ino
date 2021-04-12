4445affe489d899b544c41f776ae1ae6f5b2c29a
trholdridge <trholdridge@gmail.com>
2021-04-12 14:27:51 -0400

    prints time taken to bulk erase

--- code/v3/bulk_erase/bulk_erase.ino
+++ code/v3/bulk_erase/bulk_erase.ino
@@ -4,6 +4,9 @@ const int CHIP_SELECT_PIN = 8;
 S25FL flash(CHIP_SELECT_PIN);  // Starts Flash class and initializes SPI
 bool completed = false;
 
+unsigned long start;
+unsigned long end;
+
 void setup() {
   Serial.begin(115200);
   while (!Serial) { delay(10); }
@@ -17,12 +20,19 @@ void setup() {
 
   Serial.print("Erasing chip... ");
   flash.erase_chip_start();
+  start = millis();
 }
 
 void loop() {
   if (!completed) {
     completed = flash.is_erase_complete();
-    if (completed) { Serial.println("Done"); }
+    if (completed) { 
+      Serial.println("Done");
+      end = millis();
+      Serial.print("Erase took ");
+      Serial.print(end-start);
+      Serial.println("ms.");
+    }
   }
   delay(10);
 }
